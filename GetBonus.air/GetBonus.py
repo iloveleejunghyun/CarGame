259f34f5274a5e4bff30a01cfdc2aab17b92a852
Yuan Wan <Wan Yuan>
2021-06-10 23:09:47 +0800

    stable

--- GetBonus.air/GetBonus.py
+++ GetBonus.air/GetBonus.py
@@ -1,17 +1,19 @@
 # -*- encoding=utf8 -*-
 __author__ = "Administrator"
 
-from airtest.core.api import *
-
 auto_setup(__file__)
-
-
+import airtest
+from airtest.core.settings import Settings as ST
+from util import *
+import sys
 #只能在未连接模拟器时使用
 # dev = connect_device("Android:///")
+ST.LOG_FILE = "log.txt"
+ST.CVSTRATEGY = ['tpl']
+ST.THRESHOLD = 0.9
 
 
 def check_start():
-    dev = connect_device("android://127.0.0.1:5037/127.0.0.1:62001?cap_method=JAVACAP&&ori_method=ADBORI&&touch_method=MINITOUCH")
     if not exists(Template(r"tpl1623061463746.png", record_pos=(-0.438, -0.807), resolution=(900, 1600))):
         while True:
             
@@ -19,101 +21,75 @@ def check_start():
             sleep(5)
             start_app("com.kairogame.android.Paddock2")
             sleep(5)
-            if wait_click(Template(r"tpl1623062470610.png", record_pos=(-0.161, 0.086), resolution=(900, 1600)),False):
+            if wait_click(Template(r"tpl1623062470610.png", record_pos=(-0.161, 0.086), resolution=(900, 1600)),1, False):
                 keyevent("KEYCODE_HOME")
                 continue
-            count = 0
-            while True:
-                pos = exists(Template(r"tpl1623061904200.png", record_pos=(0.243, 0.313), resolution=(900, 1600)))
-                if pos:
-                    touch(pos)
-                    break
-                count += 1
-                if count >= 20:
-                    break
-                sleep(1)
-            if count >= 20:
+            if not wait_click(Template(r"tpl1623061904200.png", record_pos=(0.243, 0.313), resolution=(900, 1600)), 15):
                 continue
-                
+
             sleep(20)
-            count = 0
-            while True:
-                pos = exists(Template(r"tpl1623062004226.png", record_pos=(-0.002, 0.176), resolution=(900, 1600)))
-                if pos:
-                    touch(pos)
-                    break
-                
-                count += 1
-                if count >= 20:
-                    break
-                sleep(1)
-            if count >= 20:
+
+            if not wait_click(Template(r"tpl1623062004226.png", record_pos=(-0.002, 0.176), resolution=(900, 1600)), 20):
                 continue
 
             break
     return True
        
 
-def wait_click(temp_list, disapear=True):
-    if type(temp_list) is not list:
-        temp_list = [temp_list]
-    for i in range(15):
-        find = False
-        for temp in temp_list:
-            pos =  exists(temp)
-            if pos:
-                try:
-                    touch(pos)
-                    find = True
-                    sleep(2)
-                    if disapear == False:
-                        return True
-                except:
-                    pass
-        if find == False:
-            return False
-
-        
 def click_dialog():
-    wait_click(Template(r"tpl1622893722615.png", record_pos=(0.369, 0.618), resolution=(900, 1600)))
-    wait_click(Template(r"tpl1622984198757.png", record_pos=(0.369, 0.618), resolution=(900, 1600)))
-    wait_click(Template(r"tpl1623019985426.png", record_pos=(0.0, -0.06), resolution=(900, 1600)))
-    wait_click(Template(r"tpl1623020125057.png", record_pos=(-0.009, 0.079), resolution=(900, 1600)))
 
+    wait_click([Template(r"tpl1623020125057.png", record_pos=(-0.009, 0.079), resolution=(900, 1600)),Template(r"tpl1623019985426.png", record_pos=(0.0, -0.06), resolution=(900, 1600)),Template(r"tpl1622893722615.png", record_pos=(0.369, 0.618), resolution=(900, 1600)), Template(r"tpl1622984198757.png", record_pos=(0.369, 0.618), resolution=(900, 1600)),Template(r"tpl1623323573700.png", record_pos=(0.417, 0.113), resolution=(900, 1600))], 3)
 
-    wait_click(Template(r"tpl1622895401210.png", record_pos=(-0.322, 0.137), resolution=(900, 1600)))
-    
 
 
 def getBonus():
     click_dialog()
-    wait_click(Template(r"tpl1622984917583.png", record_pos=(0.371, 0.618), resolution=(900, 1600)))
+    wait_click(Template(r"tpl1622984917583.png", record_pos=(0.371, 0.618), resolution=(900, 1600)), 3)
     
-    if not exists(Template(r"tpl1623063985838.png", record_pos=(0.278, 0.18), resolution=(900, 1600))) and not exists(Template(r"tpl1623064016297.png", record_pos=(0.351, 0.183), resolution=(900, 1600))):
-        wait_click(Template(r"tpl1622981912990.png", record_pos=(0.356, 0.154), resolution=(900, 1600)))
+    if exists(Template(r"tpl1623147781059.png", record_pos=(0.291, 0.178), resolution=(900, 1600))):
+
+        wait_click(Template(r"tpl1623147781059.png", record_pos=(0.291, 0.178), resolution=(900, 1600)), 3, False)
         count = 0
 
         while not exists(Template(r"tpl1622981974455.png", record_pos=(-0.422, -0.826), resolution=(900, 1600))) and count < 12:
-            sleep(10)
-            keyevent("KEYCODE_BACK")
+#             keyevent("KEYCODE_BACK")
             count += 1
-        wait_click(Template(r"tpl1622981974455.png", record_pos=(-0.422, -0.826), resolution=(900, 1600)))
-        for i in range(14):
+            sleep(10)
+
+            
+        if not wait_click(Template(r"tpl1622981974455.png", record_pos=(-0.422, -0.826), resolution=(900, 1600)), 3):
+            sys.stderr.write("can't find close advertisement")
+        wait_click(Template(r"tpl1623229848480.png", record_pos=(0.267, 0.811), resolution=(900, 1600)), 3)
+        sleep(5)
+        wait_click(Template(r"tpl1622981974455.png", record_pos=(-0.422, -0.826), resolution=(900, 1600)), 1)
+        click_dialog()
+        for i in range(13):
             sleep(60)
             print(f"waited for {i+1} minutes")
         
 def collect_assets():
     click_dialog()
-    wait_click(Template(r"tpl1623066268636.png", record_pos=(-0.059, -0.658), resolution=(900, 1600)), False)
-    wait_click(Template(r"tpl1623066281804.png", record_pos=(0.07, -0.593), resolution=(900, 1600)), False)
-    wait_click(Template(r"tpl1623066294618.png", record_pos=(0.194, -0.532), resolution=(900, 1600)), False)
-    wait_click(Template(r"tpl1623066308919.png", record_pos=(0.32, -0.468), resolution=(900, 1600)), False)
+    wait_click(Template(r"tpl1623066268636.png", record_pos=(-0.059, -0.658), resolution=(900, 1600)), 1, False)
+    wait_click(Template(r"tpl1623066281804.png", record_pos=(0.07, -0.593), resolution=(900, 1600)), 1, False)
+    wait_click(Template(r"tpl1623066294618.png", record_pos=(0.194, -0.532), resolution=(900, 1600)), 1, False)
+    wait_click(Template(r"tpl1623066308919.png", record_pos=(0.32, -0.468), resolution=(900, 1600)), 1, False)
 
-# print(wait_click(Template(r"tpl1623062470610.png", record_pos=(-0.161, 0.086), resolution=(900, 1600)),False))
+# while True:
+#     pos = exists(Template(r"tpl1622981974455.png", record_pos=(-0.422, -0.826), resolution=(900, 1600), threshold=0.8))
+#     if pos:
+#         print(pos)
+dev = connect_device("android://127.0.0.1:5037/127.0.0.1:62001?cap_method=JAVACAP&&ori_method=ADBORI&&touch_method=MINITOUCH")
 for i in range(1000):
-    check_start()
-    sleep(5)
-    collect_assets()
-    getBonus()
+    try:
+        check_start()
+        sleep(5)
+        collect_assets()
+        getBonus()
+    except Exception as e:
+        print("error", str(e))
+        if type(e) is airtest.core.error.AdbShellError:
+            sys.stderr.write("需要重启模拟器")
+            break
+        pass
     
 
---
+++ GetBonus.air/util.py
@@ -0,0 +1,70 @@
+# -*- encoding=utf8 -*-
+__author__ = "Administrator"
+
+
+from airtest.core.api import *
+from airtest.core.api import *
+from airtest.aircv import *
+from airtest.core.api import *
+from airtest.cli.parser import cli_setup
+from airtest.core.settings import Settings as ST
+
+
+def match_in_predict_area(template,screen=None,rect=None):
+    if screen is None:
+        screen=G.DEVICE.snapshots()
+    if rect is None:
+        return template.match_in(screen)
+    if not isinstance(rect,(list,tuple)):
+        raise Exception("crop a image,rect should be a list")
+    else:
+        predict_screen=aircv.crop_image(screen,rect)
+        focus_pos=template.match_in(predict_screen)
+        if not focus_pos:
+            return False
+        else:
+            return focus_pos[0]+rect[0],focus_pos[1]+rect[1]
+
+import time
+def mutiple_exists(targets,area=None,threshold=0.80,rgb=False,inti=5):
+    if (G.DEVICE.display_info['orientation']%2):
+        height=G.DEVICE.display_info['height']
+        width=G.DEVICE.display_info['width']
+    else:
+        height = G.DEVICE.display_info['height']
+        width = G.DEVICE.display_info['width']
+    
+    for i in range(inti):
+        fullScreen=G.DEVICE.snapshot()
+        for target in targets:
+            if target:
+                focus_pos=match_in_predict_area(target, fullScreen,area)
+#                 print(time.time())
+                if focus_pos:
+                    ref=targets.index(target)
+                    return ref,focus_pos
+        sleep(0.2)
+    return -1,None
+
+def wait_click(temp_list, search_times=1, disapear=True):
+    if type(temp_list) is not list:
+        temp_list = [temp_list]
+    res = False
+    cur_times = 0
+    while cur_times < search_times:
+        cur_times += 1
+        ref, pos = mutiple_exists(temp_list)
+        
+        if ref > -1:
+            
+            touch(pos)
+            print(f"touch {pos}")
+            res = True
+            sleep(2)
+            if disapear == False:
+                return res
+        else:
+            if res == True: #如果之前找到了就返回。没有找到过就继续找. 这里无法处理多图重复出现之间时间间隔过大的情况
+                break
+        
+    return res
